dad89907402a8f813469d10ceb7f03a8ea20a2e5
bilalzafar-official <bilalcodes@gmail>
2025-02-23 21:28:38 +0500

    about let const var

--- 01basic.js
+++ 01basic.js
@@ -1,1 +1,33 @@
-console.log("bilal");
+const accountId = 12456
+let accountEmail = "@gmail.com"
+var accountPassword = 2345
+title = "banking"
+
+console.table([accountEmail,accountId,accountPassword,title])
+
+//  accountId = 12456
+ accountEmail = "change@gmail.com"
+ accountPassword = 23452345
+title = "funding"
+
+console.table([accountEmail,accountPassword,title])
+
+
+let age = 15
+
+if (age > 10) {
+    let check = 123456
+    console.log(check);
+    
+}
+
+ check = 98765;
+console.log(check);
+
+
+/*
+const cannot be change
+let in scope cannot change its value
+var everywhere change and we shouldnot be used
+
+*/
