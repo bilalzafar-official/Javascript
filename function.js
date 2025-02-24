f381150ab93cc5792d9972a56ae1dafdc71eed43
bilalzafar-official <bilalcodes@gmail>
2025-02-24 19:37:26 +0500

    functions and scope

---
+++ function.js
@@ -0,0 +1,59 @@
+
+function addNums(num1, num2) {
+    return result = num1 + num2;
+}
+
+// console.log(addNums(5,5)) // 10
+
+function concat(...nums) {
+    return result = nums
+}
+
+// console.log([100, 200, 300, 400]) // 100, 200, 300, 400;
+
+// const user = {
+//     name: 'John',
+//     age: 30
+// }
+
+function object({ name, age }) {
+    // console.log(`My name is ${name} and I am ${age} years old`);
+}
+
+object({
+    name: 'ali',
+    age: 30
+});
+
+var c = 300
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a) // 10
+// console.log(b) // 20
+// console.log(c) // 30;
+
+
+addone(5)
+function addone(num) {
+    return num + 1
+}
+
+const addtwo= function(num) {
+    return num + 1
+}
+addtwo(5)
+
+
+if (true) {
+    const user="bilal"
+    if (true) {
+     const   website = "youtube"
+     console.log(user + " is a developer and he has a website called " + website);
+     
+    }
+    // console.log(website)
+}
